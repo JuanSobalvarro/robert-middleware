1e21fa8da9d4794c578636efdc015ee2dde8d9ae
Juan Sobalvarro <sobalvarrog.juans@gmail.com>
2026-07-24 15:05:46 -0600

    feat: SIGNAL handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,23 @@
+#include <csignal>
+#include <condition_variable>
+#include <mutex>
+#include <atomic>
+#include <iostream>
 #include <stdexcept>
 #include <string>
 
 #include "server/server.hpp"
+
+std::mutex g_wait_mutex;
+std::condition_variable g_wait_cv;
+std::atomic<bool> g_shutdown_requested{false};
+
+void signal_handler(int signum)
+{
+    std::cout << "\n[MAIN] Interrupt signal (" << signum << ") received. Initiating graceful shutdown...\n";
+    g_shutdown_requested = true;
+    g_wait_cv.notify_all();
+}
 
 int main(int argc, char* argv[])
 {
@@ -10,10 +26,13 @@ int main(int argc, char* argv[])
         throw std::invalid_argument("Usage: program robots_conf_filepath users_filepath");
     }
 
+    std::signal(SIGINT, signal_handler);
+    std::signal(SIGTERM, signal_handler);
+
     const std::string& robots_filepath = argv[1];
     const std::string& users_filepath = argv[2];
 
-    std::cout << "RobertServer running with arguments: " << robots_filepath << std::endl;
+    std::cout << "RobertServer running with arguments: " << robots_filepath << " " << users_filepath << std::endl;
 
     robert::server::Server server("*", 42069);
     server.load_robots_from_file(robots_filepath);
@@ -21,7 +40,13 @@ int main(int argc, char* argv[])
 
     server.start();
 
-    server.wait();
+    // block main thread until a signal is received
+    {
+        std::unique_lock<std::mutex> lock(g_wait_mutex);
+        g_wait_cv.wait(lock, [] { return g_shutdown_requested.load(); });
+    }
+
+    server.stop();
 
     return 0;
 }
